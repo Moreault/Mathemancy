9c4e9a9b9e250e8720db2aa461fb518057406e50
Mathieu Moreault <seranx@gmail.com>
2022-07-04 05:10:33 -0400

    Added the Direction enum and removed Cuboid (for now)

---
+++ Mathemancy/Direction.cs
@@ -0,0 +1,19 @@
+﻿namespace ToolBX.Mathemancy;
+
+public enum Direction
+{
+    None,
+    Up,
+    Right,
+    Down,
+    Left,
+    UpLeft,
+    UpRight,
+    DownLeft,
+    DownRight,
+    Forward,
+    Backwards,
+    Random,
+    Horizontal,
+    Vertical
+}
