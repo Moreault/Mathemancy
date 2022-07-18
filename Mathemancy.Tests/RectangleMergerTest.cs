6c5d4a0c88aa888125c298dd771ec5c41939cc0f
Mathieu Moreault <seranx@gmail.com>
2022-07-18 08:04:31 -0400

    Added the RectangleMerger class which can be used to merge adjascent rectangles together.

---
+++ Mathemancy.Tests/RectangleMergerTest.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mathemancy.Tests;
+
+public class RectangleMergerTestBase : Tester<RectangleMerger>
+{
+
+}
+
+[TestClass]
+public class RectangleMergerTest
+{
+    [TestClass]
+    public class Merge : RectangleMergerTestBase
+    {
+        [TestMethod]
+        public void WhenRectanglesNull_Throw()
+        {
+            //Arrange
+            IReadOnlyList<Rectangle<float>> rectangles = null!;
+
+            //Act
+            Action action = () => Instance.Merge(rectangles);
+
+            //Assert
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void WhenContainsABunchOfDuplicatesOnTopOfEachOther_ReturnOneOfThem()
+        {
+            //Arrange
+            var rectangle = Fixture.Create<Rectangle<float>>();
+            var rectangles = new List<Rectangle<float>>
+            {
+                rectangle,
+                rectangle with {},
+                rectangle with {},
+                rectangle with {},
+                rectangle with {},
+            };
+
+            //Act
+            var result = Instance.Merge(rectangles);
+
+            //Assert
+            result.Should().BeEquivalentTo(new List<Rectangle<float>> { rectangle });
+        }
+
+        [TestMethod]
+        public void WhenASmallerRectangleIsInsideALargerRectangle_ReturnOnlyLargerRectangle()
+        {
+            //Arrange
+            var largeRectangle = new Rectangle<float>(0, 0, 100, 100);
+            var smallerRectangle = new Rectangle<float>(50, 50, 10, 10);
+            var rectangles = new List<Rectangle<float>>
+            {
+                largeRectangle,
+                smallerRectangle
+            };
+
+            //Act
+            var result = Instance.Merge(rectangles);
+
+            //Assert
+            result.Should().BeEquivalentTo(new List<Rectangle<float>> { largeRectangle });
+        }
+
+        [TestMethod]
+        public void WhenContainsTwoRectanglesNextToEachOther_Merge()
+        {
+            //Arrange
+            var rectangles = new List<Rectangle<float>>
+            {
+                new(0, 0, 50, 50),
+                new(50, 0, 50, 50)
+            };
+
+            //Act
+            var result = Instance.Merge(rectangles);
+
+            //Assert
+            result.Should().BeEquivalentTo(new List<Rectangle<float>> { new Rectangle<float>(0, 0, 100, 50) });
+        }
+
+        [TestMethod]
+        public void WhenContainsTwoRectanglesNextToEachOtherButOrderedBackwards_Merge()
+        {
+            //Arrange
+            var rectangles = new List<Rectangle<float>>
+            {
+                new(50, 0, 50, 50),
+                new(0, 0, 50, 50),
+            };
+
+            //Act
+            var result = Instance.Merge(rectangles);
+
+            //Assert
+            result.Should().BeEquivalentTo(new List<Rectangle<float>> { new Rectangle<float>(0, 0, 100, 50) });
+        }
+
+        [TestMethod]
+        public void WhenContainsThreeRectanglesNextToEachOther_Merge()
+        {
+            //Arrange
+            var rectangles = new List<Rectangle<float>>
+            {
+                new(0, 0, 50, 50),
+                new(50, 0, 50, 50),
+                new(100, 0, 50, 50)
+            };
+
+            //Act
+            var result = Instance.Merge(rectangles);
+
+            //Assert
+            result.Should().BeEquivalentTo(new List<Rectangle<float>> { new Rectangle<float>(0, 0, 150, 50) });
+        }
+
+        [TestMethod]
+        public void WhenContainsThreeRectanglesInLShape_MergeIntoTwo()
+        {
+            //Arrange
+            var rectangles = new List<Rectangle<float>>
+            {
+                new(0, 0, 50, 50),
+                new(50, 0, 50, 50),
+                new(0, 50, 50, 50)
+            };
+
+            //Act
+            var result = Instance.Merge(rectangles);
+
+            //Assert
+            result.Should().BeEquivalentTo(new List<Rectangle<float>>
+            {
+                new(0,0, 100, 50),
+                new(0, 50, 50, 50)
+            });
+        }
+
+        [TestMethod]
+        public void WhenContainsABunchOfSmallRectanglesInABiggerRectangularShape_ReturnSingleRectangle()
+        {
+            //Arrange
+            var rectangles = new List<Rectangle<float>>
+            {
+                new(0, 0, 50, 50),
+                new(50, 0, 50, 50),
+                new(100, 0, 50, 50),
+                new(150, 0, 50, 50),
+
+                new(0, 50, 50, 50),
+                new(50, 50, 50, 50),
+                new(100, 50, 50, 50),
+                new(150, 50, 50, 50),
+            };
+
+            //Act
+            var result = Instance.Merge(rectangles);
+
+            //Assert
+            result.Should().BeEquivalentTo(new List<Rectangle<float>> { new Rectangle<float>(0, 0, 200, 100) });
+        }
+
+        [TestMethod]
+        public void WhenContainsABunchOfSmallRectanglesWithOneMissingOnSecondLine_ReturnThreeRectangles()
+        {
+            //Arrange
+            var rectangles = new List<Rectangle<float>>
+            {
+                new(0, 0, 50, 50),
+                new(50, 0, 50, 50),
+                new(100, 0, 50, 50),
+                new(150, 0, 50, 50),
+
+                new(0, 50, 50, 50),
+                new(50, 50, 50, 50),
+                new(150, 50, 50, 50),
+            };
+
+            //Act
+            var result = Instance.Merge(rectangles);
+
+            //Assert
+            result.Should().BeEquivalentTo(new List<Rectangle<float>>
+            {
+                new(0,0, 200, 50),
+                new(0,50, 100, 50),
+                new(150,50, 50, 50)
+            });
+        }
+
+        [TestMethod]
+        public void WhenTwoRectanglesAppart_ReturnSameTwoRectanles()
+        {
+            //Arrange
+            var rectangles = new List<Rectangle<float>>
+            {
+                new(0, 0, 50, 50),
+                new(100, 100, 50, 50)
+            };
+
+            //Act
+            var result = Instance.Merge(rectangles);
+
+            //Assert
+            result.Should().BeEquivalentTo(rectangles);
+        }
+
+        [TestMethod]
+        public void WhenTwoRectanglesAppartButShareOneVertex_ReturnSameTwoRectangles()
+        {
+            //Arrange
+            var rectangles = new List<Rectangle<float>>
+            {
+                new(0, 0, 50, 50),
+                new(50, 50, 50, 50)
+            };
+
+            //Act
+            var result = Instance.Merge(rectangles);
+
+            //Assert
+            result.Should().BeEquivalentTo(rectangles);
+        }
+    }
+}
---
+++ Mathemancy/RectangleMerger.cs
@@ -0,0 +1,78 @@
+﻿namespace ToolBX.Mathemancy;
+
+public interface IRectangleMerger
+{
+    /// <summary>
+    /// Merges adjascent rectangles into as few rectangles as possible.
+    /// </summary>
+    IReadOnlyList<Rectangle<float>> Merge(IReadOnlyList<Rectangle<float>> rectangles);
+}
+
+[AutoInject]
+public class RectangleMerger : IRectangleMerger
+{
+    public IReadOnlyList<Rectangle<float>> Merge(IReadOnlyList<Rectangle<float>> rectangles)
+    {
+        if (rectangles == null) throw new ArgumentNullException(nameof(rectangles));
+
+        if (rectangles.Any(x => x.Width == 0 || x.Height == 0))
+            throw new ArgumentException($"Can't merge rectangles : This method does not merge rectangles with a width or height of 0 but {rectangles.Count(x => x.Width == 0 || x.Height == 0)} out of {rectangles.Count} had an invalid width or height.");
+
+        var output = rectangles.Distinct().ToList();
+
+        foreach (var current in output.Where(current => output.Any(x => x != current && x.Left <= current.Left && x.Right >= current.Right && x.Top <= current.Top && x.Bottom >= current.Bottom)).ToList())
+            output.Remove(current);
+            
+        while (true)
+        {
+            foreach (var current in output.ToList())
+            {
+                if (output.Any(x => current.TopRight == x.TopLeft && current.BottomRight == x.BottomLeft))
+                {
+                    var other = output.Single(x => current.TopRight == x.TopLeft && current.BottomRight == x.BottomLeft);
+                    var newRectangle = new Rectangle<float>(current.TopLeft, other.Right - current.Left, current.Height);
+
+                    output.Remove(current);
+                    output.Remove(other);
+                    output.Add(newRectangle);
+                    return Merge(output);
+                }
+
+                if (output.Any(x => x.TopRight == current.TopLeft && x.BottomRight == current.BottomLeft))
+                {
+                    var other = output.Single(x => x.TopRight == current.TopLeft && x.BottomRight == current.BottomLeft);
+                    var newRectangle = new Rectangle<float>(other.TopLeft, current.Right - other.Left, current.Height);
+
+                    output.Remove(current);
+                    output.Remove(other);
+                    output.Add(newRectangle);
+                    return Merge(output);
+                }
+
+                if (output.Any(x => current.BottomLeft == x.TopLeft && current.BottomRight == x.TopRight))
+                {
+                    var other = output.Single(x => current.BottomLeft == x.TopLeft && current.BottomRight == x.TopRight);
+                    var newRectangle = new Rectangle<float>(current.TopLeft, current.Width, other.Bottom - current.Top);
+
+                    output.Remove(current);
+                    output.Remove(other);
+                    output.Add(newRectangle);
+                    return Merge(output);
+                }
+
+                if (output.Any(x => x.BottomLeft == current.TopLeft && x.BottomRight == current.TopRight))
+                {
+                    var other = output.Single(x => x.BottomLeft == current.TopLeft && x.BottomRight == current.TopRight);
+                    var newRectangle = new Rectangle<float>(other.TopLeft, current.Width, current.Bottom - other.Top);
+
+                    output.Remove(current);
+                    output.Remove(other);
+                    output.Add(newRectangle);
+                    return Merge(output);
+                }
+
+            }
+            return output;
+        }
+    }
+}
